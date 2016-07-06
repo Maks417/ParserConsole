6ca9803722234827fb8ddfafede51c92971d4358
Maxim Davydov <MDavydov@domain.corp>
2016-07-06 15:18:14 +0300

    Working methods have been placed to separate class

---
+++ Parser.cs
@@ -0,0 +1,48 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ParserConsoleApplication
+{
+    public class Parser
+    {
+        public byte[] _page { get; private set; }
+        public Parser(string path)
+        {
+            Init(path);
+        }
+        private async void Init(string path)
+        {
+            _page = await DownloadPage(path);
+        }
+        public async Task<List<string>> GetDomains()
+        {
+            if (_page != null)
+            {
+                List<string> domains = new List<string>();
+
+                string result = System.Text.Encoding.UTF8.GetString(_page);
+
+                HtmlDocument html = new HtmlDocument();
+                html.LoadHtml(result);
+                foreach (var item in html.DocumentNode.SelectNodes("//*[@class='domain']"))
+                {
+                    domains.Add(item.InnerHtml);
+                }
+                return domains;
+            }
+            Console.WriteLine("No domains");
+            return null;
+        }
+        private async Task<byte[]> DownloadPage(string path)
+        {
+            var client = new WebClient();
+            var link = "https://direct.yandex.ru/search?text=" + path;
+            Uri uri = new Uri(link);
+            var page = client.DownloadDataTaskAsync(uri);
+            return page.Result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿using HtmlAgilityPack;
-using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace ParserConsoleApplication
 {
@@ -10,40 +6,11 @@ namespace ParserConsoleApplication
     {
         static void Main(string[] args)
         {
-            var page = DownloadPage("garpix.ru");
-            var domains = GetDomains(page.Result);
+            var parser = new Parser("svetlanatex.ru");
+            var domains = parser.GetDomains();
 
             Console.WriteLine("Common numbers of domains: {0}",domains.Result.Count);
             Console.ReadKey();
-        }
-
-        private static async Task<List<string>> GetDomains(byte[] page)
-        {
-            if (page != null)
-            {
-                List<string> domains = new List<string>();
-
-                string result = System.Text.Encoding.UTF8.GetString(page);
-
-                HtmlDocument html = new HtmlDocument();
-                html.LoadHtml(result);
-                foreach (var item in html.DocumentNode.SelectNodes("//*[@class='domain']"))
-                {
-                    domains.Add(item.InnerHtml);
-                }
-                return domains;
-            }
-            Console.WriteLine("No domains");
-            return null;
-        }
-
-        private static async Task<byte[]> DownloadPage(string path)
-        {
-            var client = new WebClient();
-            var link = "https://direct.yandex.ru/search?text=" + path;
-            Uri uri = new Uri(link);            
-            var page = client.DownloadDataTaskAsync(uri);
-            return page.Result;
         }
     }
 }
