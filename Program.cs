8b43b3ce60d5ae7a9ef33bc97c137a24ac6f290f
Maxim Davydov <MDavydov@domain.corp>
2016-07-07 11:24:05 +0300

    Added TODO's

--- Program.cs
+++ Program.cs
@@ -6,10 +6,18 @@ namespace ParserConsoleApplication
     {
         static void Main(string[] args)
         {
-            var parser = new Parser("svetlanatex.ru");
+            #region [.input domain from keyboard - Replace to file parser]
+            Console.WriteLine("Please, insert concrete domain:");
+            var input = Console.ReadLine();
+            Console.WriteLine(); 
+            #endregion
+            
+            var parser = new Parser(input);
             var domains = parser.GetDomains();
 
-            Console.WriteLine("Common numbers of domains: {0}",domains.Result.Count);
+            //TODO: Add select distinct(?) and save to file
+
+            Console.WriteLine("{1}Common numbers of domains: {0}",domains.Result.Count, System.Environment.NewLine);
             Console.ReadKey();
         }
     }
